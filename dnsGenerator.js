201e32b4ebaabae279661d68b3d6ce4cf8f869bc
unknown <argelio.baca@gmail.com>
2023-06-01 12:11:17 -0600

    completed domain name generator project

---
+++ dnsGenerator.js
@@ -0,0 +1,14 @@
+let pronoun = ['the','our'];
+let adj = ['great', 'big' ];
+let noun = ['jogger','racoon'];
+let extensions = ['.us', '.io', '.com', '.edu']
+
+for(let i = 0; i < pronoun.length; i++){
+  for(let j = 0; j < adj.length; j++){
+    for(let k = 0; k < noun.length; k++){
+      for(let l = 0; l < extensions.length; l++){
+        console.log(`${pronoun[i]}${adj[j]}${noun[k]}${extensions[l]}`)
+      }
+    }
+  }
+}
